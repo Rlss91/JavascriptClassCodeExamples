8a401304e293d443a480f0d3e8672d88e3e88baf
Rlss <rlss91@gmail.com>
2020-01-30 20:50:27 +0200

    server

--- server/app.js
+++ server/app.js
@@ -1,10 +1,13 @@
 const path = require('path')
 const express = require('express')
 const app = express()
-var cors = require('cors')
+const cors = require('cors')
+const bodyParser = require('body-parser')
 const PORT = 3000
 
 app.use(cors())
+// parse various different custom JSON types as JSON
+app.use(bodyParser.json({ type: 'application/json' }))
 
 app.get('/', (req, res) => {
     console.log(req.query)
@@ -24,7 +27,29 @@ app.get('/arr', (req, res) => {
         res.json({ error: 'the array starts with 0 and ands in 2' })
 })
 
+app.post('/arr', (req, res) => {
+    let arr = [
+        [1, 2, 3, 4, 5],
+        [2, 3, 4, 5, 6],
+        [3, 4, 5, 6, 7]
+    ]
+    let index = parseInt(req.body.index)
+    if (index >= 0 && index < arr.length)
+        res.json(arr[index])
+    else
+        res.json({ error: 'the array starts with 0 and ands in 2' })
+})
+
 app.get('/json', (req, res) => {
+    let arr = [
+        [1, 2, 3, 4, 5],
+        [2, 3, 4, 5, 6],
+        [3, 4, 5, 6, 7]
+    ]
+    res.json(arr)
+})
+
+app.post('/json', (req, res) => {
     let arr = [
         [1, 2, 3, 4, 5],
         [2, 3, 4, 5, 6],
